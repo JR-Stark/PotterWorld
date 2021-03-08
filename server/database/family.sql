424645f2fc8fd524cf2686899f4177aa9655cda4
Jack-Jerry-Stark <jerry.estelle.dumont@gmail.com>
2021-03-08 13:54:19 +0100

    correction family.sql

--- server/database/family.sql
+++ server/database/family.sql
@@ -23,6 +23,16 @@ SET row_security = off;
 -- Dependencies: 213
 -- Data for Name: family; Type: TABLE DATA; Schema: public; Owner: postgres
 --
+-- --------------------------------------------
+-- table family
+-- --------------------------------------------
+
+CREATE TABLE IF NOT EXISTS "family" (
+    "id" SERIAL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "created_at" TIMESTAMP NOT NULL DEFAULT NOW(),
+    "updated_at" TIMESTAMP
+);
 
 INSERT INTO "family" ("id", "name", "created_at", "updated_at") VALUES
 (1, 'Londubat', '2020-08-27 00:05:14.225918', NULL),
