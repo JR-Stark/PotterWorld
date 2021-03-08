a48bc27ed1cc775c1899525a6ab1d90a6150a033
Jack-Jerry-Stark <jerry.estelle.dumont@gmail.com>
2021-03-08 14:02:14 +0100

    correction data-1599341898990.sql

--- server/database/family.sql
+++ server/database/family.sql
@@ -1,16 +1,16 @@
 BEGIN;
 
-DROP TABLE IF EXISTS "family";
+-- DROP TABLE IF EXISTS "family";
 -- --------------------------------------------
 -- table family
 -- --------------------------------------------
 
-CREATE TABLE IF NOT EXISTS "family" (
-    "id" SERIAL PRIMARY KEY,
-    "name" TEXT NOT NULL,
-    "created_at" TIMESTAMP NOT NULL DEFAULT NOW(),
-    "updated_at" TIMESTAMP
-);
+-- CREATE TABLE IF NOT EXISTS "family" (
+--     "id" SERIAL PRIMARY KEY,
+--     "name" TEXT NOT NULL,
+--     "created_at" TIMESTAMP NOT NULL DEFAULT NOW(),
+--     "updated_at" TIMESTAMP
+-- );
 
 INSERT INTO "family" ("id", "name", "created_at", "updated_at") VALUES
 (1, 'Londubat', '2020-08-27 00:05:14.225918', NULL),
